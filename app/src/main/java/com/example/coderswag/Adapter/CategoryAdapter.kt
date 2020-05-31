d38aba45ae162e04ec3a87e0d035043a3010004a
sureshsalem <suresh4ueee@gmail.com>
2020-05-31 22:55:42 +0530

    RecyclerView

--- app/src/main/java/com/example/coderswag/Adapter/CategoryAdapter.kt
+++ app/src/main/java/com/example/coderswag/Adapter/CategoryAdapter.kt
@@ -11,10 +11,7 @@ import com.example.coderswag.Model.Category
 import com.example.coderswag.R
 import kotlinx.android.synthetic.*
 
-class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
-
-    val context = context
-    val categories = categories
+class CategoryAdapter(val context: Context, val categories: List<Category>) : BaseAdapter() {
 
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.coderswag.Adapter.CategoryAdapter
+import com.example.coderswag.Adapter.CategoryRecycleAdapter
 import com.example.coderswag.Model.Category
 import com.example.coderswag.R
 import com.example.coderswag.Services.DataServices
@@ -12,16 +17,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         /*adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,DataServices.Categories)*/
-        adapter = CategoryAdapter(this, DataServices.Categories)
+        adapter = CategoryRecycleAdapter(this, DataServices.Categories)
         categoriesListView.adapter = adapter
-
+        val layoutManager = LinearLayoutManager(this)
+        categoriesListView.layoutManager =layoutManager
+        categoriesListView.setHasFixedSize(true)
+        
 
 
 
